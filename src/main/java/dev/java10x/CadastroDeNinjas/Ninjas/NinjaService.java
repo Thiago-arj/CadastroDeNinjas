ae3bbe829210ed2f7cbcb6890492389cf1ca53ec
Thiago-arj <Thiagoaraujo1102@icloud.com>
2026-08-26 21:01:00 -0300

    feat: Rota de findById adicionada

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -3,6 +3,7 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class NinjaService {
@@ -13,10 +14,17 @@ public class NinjaService {
         this.ninjaRepository = ninjaRepository;
     }
 
-    //Listar todos os  ninjas
+    //Listar todos os ninjas
     public List<NinjaModel> listarNinjas() {
         return ninjaRepository.findAll();
     }
+
+    //Listar todos os ninjas por ID
+    public NinjaModel listarNinjasPorId(Long id) {
+        Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
+        return ninjaPorId.orElse(null);
+    }
+
 
 
 }
