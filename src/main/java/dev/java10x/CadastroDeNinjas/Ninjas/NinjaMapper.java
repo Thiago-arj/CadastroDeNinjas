e8e92c1299d56a63c84f97a9027053cdc0e8ee63
Thiago-arj <Thiagoaraujo1102@icloud.com>
2026-09-02 22:05:46 -0300

    chore: Inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade X dto

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,32 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setImg_url(ninjaDTO.getImg_url());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel) {
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaDTO.getId());
+        ninjaDTO.setNome(ninjaDTO.getNome());
+        ninjaDTO.setEmail(ninjaDTO.getEmail());
+        ninjaDTO.setIdade(ninjaDTO.getIdade());
+        ninjaDTO.setImg_url(ninjaDTO.getImg_url());
+        ninjaDTO.setRank(ninjaDTO.getRank());
+        ninjaDTO.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaDTO;
+    }
 }
