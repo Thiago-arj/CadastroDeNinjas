85207be96914812bbe3f5c231f310958ac033b61
Thiago-arj <Thiagoaraujo1102@icloud.com>
2026-09-10 16:01:41 -0300

    fix: tira a responsabilidade do meu model e passa para o DTO, usando Streams no mapper

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -19,13 +19,13 @@ public class NinjaMapper {
 
     public NinjaDTO map(NinjaModel ninjaModel) {
         NinjaDTO ninjaDTO = new NinjaDTO();
-        ninjaDTO.setId(ninjaDTO.getId());
-        ninjaDTO.setNome(ninjaDTO.getNome());
-        ninjaDTO.setEmail(ninjaDTO.getEmail());
-        ninjaDTO.setIdade(ninjaDTO.getIdade());
-        ninjaDTO.setImg_url(ninjaDTO.getImg_url());
-        ninjaDTO.setRank(ninjaDTO.getRank());
-        ninjaDTO.setMissoes(ninjaDTO.getMissoes());
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setImg_url(ninjaModel.getImg_url());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
 
         return ninjaDTO;
     }
